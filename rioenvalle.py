8b8cc467da1a6944928613b8d4b9def4948c582e
Unai <unai.sanmiguel@gmail.com>
2023-12-07 11:42:57 +0100

    use functions in rioenvalle

--- rioenvalle.py
+++ rioenvalle.py
@@ -1,3 +1,54 @@
+from typing import List, Tuple
+
+def get_extreme(matrix: List[List[int]], max_: bool=True) -> Tuple[int, int, int]:
+    if max_:
+        func = max
+    else:
+        func = min
+    extremes = []
+    jextremes = []
+    for f in matrix:
+        extreme = func(f)
+        jextreme = f.index(extreme)
+        extremes.append(extreme)
+        jextremes.append(jextreme)
+    value = func(extremes)
+    ivalue = extremes.index(value)
+    jvalue = jextremes[ivalue]
+
+    return value, ivalue, jvalue
+
+
+def get_max(matrix: List[List[int]]) -> Tuple[int, int, int]:
+    maxis = []
+    jmaxis = []
+    for f in matrix:
+        maxi = max(f)
+        jmaxi = f.index(maxi)
+        maxis.append(maxi)
+        jmaxis.append(jmaxi)
+    top = max(maxis)
+    itop = maxis.index(top)
+    jtop = jmaxis[itop]
+
+    return top, itop, jtop
+
+def get_min(matrix: List[List[int]]) -> Tuple[int, int, int]:
+    minis = []
+    jminis = []
+    for f in matrix:
+        mini = min(f)
+        jmini = f.index(mini)
+        minis.append(mini)
+        jminis.append(jmini)
+    bot = min(minis)
+    ibot = minis.index(bot)
+    jbot = jminis[ibot]
+
+    return bot, ibot, jbot
+
+
+
 fichero_entrada = "rioenvalle.in"
 
 t = []
@@ -11,42 +62,27 @@ with open(fichero_entrada, "rt") as f:
         for j, v in enumerate(temp):
             t[i][j] = int(v)
 
-maxis = []
-imaxis = []
-for fila in t:
-    maxi = max(fila)
-    maxis.append(maxi)
-    imaxis.append(fila.index(maxi))
+# maxis = []
+# imaxis = []
+# for fila in t:
+#     maxi = max(fila)
+#     maxis.append(maxi)
+#     imaxis.append(fila.index(maxi))
 
-top = max(maxis)
-itop = maxis.index(top)
-jtop = imaxis[itop]
+# top = max(maxis)
+# itop = maxis.index(top)
+# jtop = imaxis[itop]
 
+top, itop, jtop = get_max(t)
 
-t_peque = []
-ij_peque = [tuple(range(itop-1, itop+2)), tuple(range(jtop-1, jtop+2))]
-for i, fila in enumerate(t[itop-1:itop+2]):
-    t_peque.append(fila[jtop-1:jtop+2])
+t_peque = [f[jtop-1:jtop+2] for f in t[itop-1:itop+2]]
+bot, ibot, jbot = get_min(t_peque)
 
-minis = []
-iminis = []
-for fila in t_peque:
-    mini = min(fila)
-    minis.append(mini)
-    iminis.append(fila.index(mini))
+while 
 
-bot = min(minis)
-ibot = minis.index(mini)
-jbot = ij_peque[1][ibot]
-
-print(f"{maxis = }")
-print(f"{imaxis = }")
 print(f"{top = }, at i, j = {itop}, {jtop}")
 print(f"check: {t[itop][jtop] = }")
 
 print(f"{t_peque = }")
-print(f"{ij_peque = }")
 
-print(f"{minis = }")
-print(f"{iminis = }")
 print(f"{bot = }, at i, j = {ibot}, {jbot}")
